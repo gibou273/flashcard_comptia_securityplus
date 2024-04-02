857db54d27f3b17edb1888a01bcdc3eb9da95362
gibou273 <101921033+gibou273@users.noreply.github.com>
2024-04-03 00:56:20 +0200

    Remove some lines of commented code from the send_email.py file.

--- send_email.py
+++ send_email.py
@@ -16,7 +16,5 @@ def send_mail(message):
         # Log in to your email account
         connection.login(user=my_email_address, password=my_app_password)
         # Send email
-        # message_to_send = "Hello!\nYou can now go and take the CompTIA Network+ Exams.\n\nRegards"
-        # message_to_send = "Hello!\nYou can now go and take the CompTIA Network+ Exams.\n\nRegards"
         connection.sendmail(from_addr=my_email_address, to_addrs=to_email_address,
                             msg=f"Subject:Congratulations\n\nHello!\n{message}\n\nRegards\nYour Favourite Security+ App")
